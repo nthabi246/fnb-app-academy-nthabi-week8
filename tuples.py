750934dbd7049cfabf8015c39eef7f0430cd0af4
nthabi246 <thabelengm@gmail.com>
2025-07-02 19:47:37 +0200

    week8

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# Tuples
+
+my_tuples =(1,2,3,4,5)
+
+print(my_tuples)
+print(my_tuples[0])
+print(my_tuples[2])
+print(my_tuples[-1])
+
+tuple1 =(1,2,3)
+tuple2 =(4,5,6)
+
+conc_tuple =tuple1 + tuple2
+rep_tuple = tuple1 * 3
+
+print(rep_tuple)
